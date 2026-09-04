7b3b79376626e3768a3d38df43b67b87c16431f4
maoxian <wjll8723@163.com>
2026-09-04 14:38:53 +0800

    去掉 泰拉硬依赖

--- modinfo.lua
+++ modinfo.lua
@@ -7,9 +7,6 @@ priority = -1
 dst_compatible = true
 all_clients_require_mod = true
 client_only_mod = false
-dependencies = {
-    {workshop = "2526778484"},
-}
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
