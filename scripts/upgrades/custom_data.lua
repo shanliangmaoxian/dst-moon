4cca8829c6717fe94e5def87d06879a882e25055
zkip <zkiplan@qq.com>
2026-09-16 21:17:25 +0800

    fix: 修复注释错误

--- scripts/upgrades/custom_data.lua
+++ scripts/upgrades/custom_data.lua
@@ -6,7 +6,7 @@ return {
 			-- 例如数据在上次保存时的版本号为 3，这个列表的长度为7，则它需要经历的升级程序为该列表的 [4]、[5]、[6]、[7]
 			-- 若对应 Key 的数据结构发生了变化，只需要在这里对应的 Key 的列表末尾增加处理程序即可，不需要在使用时关心版本号
 			function (data) -- version 1
-				-- 寒月公主的击杀记忆结构发生了变化 string[] -> {prefab: string, count: 0}[]
+				-- 寒月试炼的击杀记忆结构发生了变化 string[] -> {prefab: string, count: 0}[]
 				return LMOON.map(data, function (mem_str) return {prefab = mem_str, count = 1} end)
 			end
 		}
