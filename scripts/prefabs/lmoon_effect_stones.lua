7e34423fd57f3b81931e853799ef5ab333fb809c
maoxian <wjll8723@163.com>
2026-09-21 11:53:10 +0800

    Revert "feat: 小樱无限魔力"
    This reverts commit d79be9f1c581f97bcf48b6614b4595aa694f2217.

--- scripts/prefabs/lmoon_effect_stones.lua
+++ scripts/prefabs/lmoon_effect_stones.lua
@@ -1,6 +1,5 @@
 -- reference from aria mod https://steamcommunity.com/sharedfiles/filedetails/?id=2418617371
--- announce：可选，产物炼成时的全服公告模板（%s 为玩家名）
-local function make_stone_prefab(prefab_name, effect_name, announce)
+local function make_stone_prefab(prefab_name, effect_name)
     local function fn()
         local inst = CreateEntity()
 
@@ -23,11 +22,6 @@ local function make_stone_prefab(prefab_name, effect_name, announce)
             stone.Transform:SetPosition(x, y, z)
             builder.components.inventory:GiveItem(stone)
 
-            if announce ~= nil and TheNet ~= nil then
-                local name = builder.GetDisplayName ~= nil and builder:GetDisplayName() or "玩家"
-                TheNet:Announce(string.format(announce, tostring(name)))
-            end
-
             inst:Remove()
         end
 
@@ -37,6 +31,4 @@ local function make_stone_prefab(prefab_name, effect_name, announce)
 end
 
 return 	make_stone_prefab('moon_effect_stone_hanyue_test', "Legend_HANYUE_TEST"),
-		make_stone_prefab('lmoon_effect_stone_quickcast', "lmoon_effect_quickcast"),
-		make_stone_prefab('lmoon_effect_stone_infinite_star', "Legend_infinite_star",
-			"恭喜 %s 炼成了「无限星力附魔石」！")
+		make_stone_prefab('lmoon_effect_stone_quickcast', "lmoon_effect_quickcast")
